e7141cf2dbbc4d1c22f9cbf54f0700bc8d8df50a
Viktor Forsman <forsmvi@t-4015-01.studat.chalmers.se>
2011-10-06 18:47:52 +0200

    deleted:    generateSubproblem/barPlot.m 	deleted:    generateSubproblem/firstDraftMain.m 	deleted:    generateSubproblem/secondDraftMain.m 	deleted:    generateSubproblem/solveSubProblem.m 	modified:   generateSubproblem/createSchedule.m 	modified:   generateSubproblem/feasibleSched.m 	modified:   generateSubproblem/generateObj.m 	deleted:    generateSubproblem/plotSchedules.m

--- generateSubproblem/solveSubProblem.m
+++
@@ -1,6 +0,0 @@
-function [extremePoint objValue] = solveSubProblem(objFunc, prob)
-
-prob.Model.obj = objFunc;
-prob.Model.solve();
-extremePoint = prob.Solution.x;
-objValue = prob.Solution.ojbval;
